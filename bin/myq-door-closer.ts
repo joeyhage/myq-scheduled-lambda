ef3908b922d03d333e5cf3c65d94d1bbf2405429
Joey Hage <joeyhage@outlook.com>
2022-02-26 21:15:34 -0600

    fix: fix import in main cdk file

--- bin/myq-door-closer.ts
+++ bin/myq-door-closer.ts
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 import "source-map-support/register";
-import * as cdk from "@aws-cdk/core";
+import * as cdk from "aws-cdk-lib";
 import { MyqDoorCloserStack } from "../lib/myq-door-closer-stack";
 
 const app = new cdk.App();
