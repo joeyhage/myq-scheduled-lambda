ff0b4b2106fd831b18ab6f733eba158337768aa9
joeyhage <3220319+joeyhage@users.noreply.github.com>
2023-09-19 14:20:07 -0500

    fix relative import

--- bin/myq-door-closer.ts
+++ bin/myq-door-closer.ts
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 import "source-map-support/register";
 import * as cdk from "aws-cdk-lib";
-import { MyqDoorCloserStack } from "../lib/myq-door-closer-stack";
+import { MyqDoorCloserStack } from "../lib/myq-door-closer-stack.js";
 
 const app = new cdk.App();
 new MyqDoorCloserStack(app, "MyqDoorCloserStack");
